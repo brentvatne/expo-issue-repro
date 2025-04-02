0853b338a94b4b774576f418ca912308a310c137
dkrabdev <171172447+dkrabdev@users.noreply.github.com>
2025-04-02 17:21:33 -0600

    chore: stripped everything down

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,6 @@
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
-const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  try {
-    return context.resolveRequest(context, moduleName, platform);
-  } catch (error) {
-    if (moduleName.endsWith('.js')) {
-      const tsModuleName = moduleName.replace(/\.js$/, '.ts');
-      return context.resolveRequest(context, tsModuleName, platform);
-    }
-    throw error;
-  }
-};
-
-module.exports = wrapWithReanimatedMetroConfig(config);
+module.exports = config;
